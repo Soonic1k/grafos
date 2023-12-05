43539f8ed8096ae667f51fe0ae3df103d1b33a67
Soonic1k <51142799+Soonic1k@users.noreply.github.com>
2023-12-04 22:34:22 -0300

    Ex 09 corrigido.

--- 09 - filaDePrioridade.py/filaDePrioridade.py
+++ 09 - filaDePrioridade.py/filaDePrioridade.py
@@ -1,8 +1,5 @@
 import numpy as np
 
-def fila_vazia(Q):
-    return np.all(Q == 0)
-
 def insere(Q, i):
     Q[i] = 1
     return Q
@@ -10,43 +7,60 @@ def insere(Q, i):
 def busca(Q, i):
     return Q[i]
 
-def extraiMinimo(Q):
-    minimo_indice = np.argmin(Q)
-    Q[minimo_indice] = 0
-    return minimo_indice, Q
+def extraiMinimo (Q, chave):
+    for i in range (len(Q)):
+        if Q[i] == 1:
+            min = i
+            break
+    for i in range (len(Q)):
+        if Q[i] == 1  and chave[i] < chave[min]:
+            min = i
+    Q[min] = 0
+    return min, Q 
 
-def minimo(Q):
-    minimo_indice = np.argmin(Q)
-    return minimo_indice, Q
+def minimo(Q, chave):
+    for i in range (len(Q)):
+        if Q[i] == 1:
+            min = i
+            break
+    for i in range (len(Q)):
+        if Q[i] == 1  and chave[i] < chave[min]:
+            min = i
+    return min
+
+def fila_vazia (Q):
+    for i in range (len(Q)):
+        if Q[i] == 1:
+            return False
+    return True
+
 
 # Leitura da entrada
 n, k = (int(tmp) for tmp in input().split(" "))
-Q = np.zeros(n, dtype=int)
-chaves = list (int(tmp) for tmp in input().split(" "))
+Q=[0]*n
+tmp2 = list (int(tmp) for tmp in input().split(" "))
+chave = np.array (tmp2)
 
 for _ in range(k):
     linha = input().split()
     
     if len(linha) == 1:
-        operacao = linha
+        operacao = linha[0]
     elif len(linha) == 2:
         operacao, j = linha
 
     if operacao == 'I':
-        j = int(j)
-        Q = insere(Q, j)
-        print(Q.tolist())
+        Q=insere(Q, int(j))
+        print(Q)
     elif operacao == 'B':
-        j = int(j)
-        resultado = busca(Q, j)
-        print(f"{resultado} {Q.tolist()}")
+        resultado = busca(Q, int(j))
+        print(resultado, Q)
     elif operacao == 'E':
-        indice, Q = extraiMinimo(Q)
-        print(f"{indice} {chaves[indice]} {Q.tolist()}")
+        min, Q =extraiMinimo(Q, chave)
+        print(min, chave[min], Q)
     elif operacao == 'M':
-        indice, Q = minimo(Q)
-        print(f"{indice} {chaves[indice]} {Q.tolist()}")
+        min=(minimo(Q, chave))
+        print(min, chave[min], Q)
     elif operacao == 'V':
         resultado = fila_vazia(Q)
-        print(f"{resultado} {Q.tolist()}")
-
+        print(resultado, Q)
